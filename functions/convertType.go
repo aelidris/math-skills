55fcfe65864c488f29c19a7992eff3658385e230
aelidris <smias1.1998@gmail.com>
2024-09-09 14:51:41 +0100

    math-skills

--- functions/convertType.go
+++ functions/convertType.go
@@ -1,13 +1,16 @@
 package static
 
-import "strconv"
+import (
+	"log"
+	"strconv"
+)
 
 // ConvertType converts a string to an integer. If the conversion fails, it returns 0.
 func ConvertType(s string) int {
 	num := 0                    // Initialize a variable to store the converted integer
 	num, err := strconv.Atoi(s) // Attempt to convert the string to an integer
 	if err != nil {             // If there's an error during conversion
-		return 0 // Return 0 as a default value
+		log.Fatalf("Conversion unsuccessful: failed to convert string to integer")
 	}
 
 	return num // Return the successfully converted integer
